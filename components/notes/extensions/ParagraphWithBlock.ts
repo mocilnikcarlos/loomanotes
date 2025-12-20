fdfbe9f3b20f912479d3ec3b5372c716a50b35dc
mocilnikcarlos <mocilnik.web@gmail.com>
2025-12-20 20:17:13 -0300

    fix(root): naming file

--- components/notes/extensions/ParagraphWithBlock.ts
+++ components/notes/extensions/ParagraphWithBlock.ts
@@ -1,6 +1,6 @@
 import Paragraph from "@tiptap/extension-paragraph";
 import { ReactNodeViewRenderer } from "@tiptap/react";
-import { BlockView } from "./ui/BlockView";
+import { BlockView } from "../nodeview/BlockView";
 
 export const ParagraphWithBlock = Paragraph.extend({
   draggable: false,
